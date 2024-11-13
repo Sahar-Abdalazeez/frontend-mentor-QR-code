192b0fe9ee77606aab765a976b5e5d21b0c2af1a
“sahar <sahar@aui.io>
2024-11-13 20:33:57 +0200

    Qr code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import qrCode from './qr-code.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="qr-container">
+        <img src={qrCode} alt={"QR-code  image "} />
+        <p className="title">
+          Improve your front-end <br /> skills by building projects
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="description">
+          Scan the QR code to visit Frontend
+          <br /> Mentor and take your coding skills to
+          <br /> the next level
+        </p>
+      </div>
     </div>
   );
 }
